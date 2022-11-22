39a5d591c7306dc040703ec19a3c4e2cc7f6f9e7
lucardcoder <gokalpkocer@gmail.com>
2022-11-22 05:31:52 +0000

    asg-alb added

--- main.tf
+++ main.tf
@@ -14,6 +14,10 @@ resource "aws_vpc" "vpc" {
     Terraform   = "true"
   }
 }
+
+
+
+
 #Deploy the private subnets
 
 resource "aws_subnet" "private_subnets" {
@@ -107,5 +111,107 @@ resource "aws_nat_gateway" "nat_gateway" {
   tags = {
     Name = "project_nat_gateway"
   }
+}
+
+
+module "asg" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.5.3"
+  # Autoscaling group
+  name                      = "project-asg"
+  min_size                  = 3
+  max_size                  = 10
+  desired_capacity          = 3
+  wait_for_capacity_timeout = 0
+  vpc_zone_identifier       = [for subnet in aws_subnet.public_subnets : subnet.id]
+  health_check_type         = "EC2"
+
+
+
+  # Launch template
+  launch_template_name        = "example-asg"
+  launch_template_description = "Launch template example"
+  update_default_version      = true
+  image_id                    = "ami-0b0dcb5067f052a63"
+  instance_type               = "t3.micro"
+  ebs_optimized               = false
+  enable_monitoring           = false
+  user_data                   = "IyEvYmluL2Jhc2ggCgp5dW0gaW5zdGFsbCBodHRwZCB3Z2V0IHVuemlwIGVwZWwtcmVsZWFzZSBteXNxbCAteSAKCnN1ZG8gYW1hem9uLWxpbnV4LWV4dHJhcyBpbnN0YWxsIGVwZWwgLXkgCgp5dW0gLXkgaW5zdGFsbCBodHRwczovL3JwbXMucmVtaXJlcG8ubmV0L2VudGVycHJpc2UvcmVtaS1yZWxlYXNlLTcucnBtIAoKeXVtLWNvbmZpZy1tYW5hZ2VyIC0tZW5hYmxlIHJlbWktcGhwNzQgCgp5dW0gaW5zdGFsbCBwaHAgLXkgCgp5dW0gaW5zdGFsbCBwaHAtbXlzcWwgLXkgCgogCndnZXQgaHR0cDovL3dvcmRwcmVzcy5vcmcvd29yZHByZXNzLTQuMC4zMi50YXIuZ3ogIAoKIAp0YXIgLXhmIHdvcmRwcmVzcy00LjAuMzIudGFyLmd6IC1DIC92YXIvd3d3L2h0bWwvIAoKbXYgL3Zhci93d3cvaHRtbC93b3JkcHJlc3MvKiAvdmFyL3d3dy9odG1sLyAgCgpHZXRlbmZvcmNlIAoKc2VkICdzL1NFTElOVVg9cGVybWlzc2l2ZS9TRUxJTlVYPWVuZm9yY2luZy9nJyAvZXRjL3N5c2NvbmZpZy9zZWxpbnV4IC1pICAKc2V0ZW5mb3JjZSAwIAogCmNob3duIC1SIGFwYWNoZTphcGFjaGUgL3Zhci93d3cvaHRtbC8gCiAKc3VkbyBzeXN0ZW1jdGwgcmVzdGFydCBodHRwZCAKIApzdWRvIHN5c3RlbWN0bCBlbmFibGUgaHR0cGQg"
+  target_group_arns           = module.alb.target_group_arns
+  security_groups = [
+    aws_security_group.allow_tls.id
+  ]
+}
+
+
+resource "aws_security_group" "allow_tls" {
+  name        = "terraform-testing-alb"
+  description = "Allow TLS inbound traffic"
+  vpc_id = aws_vpc.vpc.id
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+
+module "alb" {
+  source             = "terraform-aws-modules/alb/aws"
+  version            = "~> 8.0"
+  name               = "my-alb"
+  load_balancer_type = "application"
+  enable_cross_zone_load_balancing = true
+  vpc_id             = aws_vpc.vpc.id
+  subnets = [for subnet in aws_subnet.public_subnets : subnet.id]
+
+  security_groups = [
+    aws_security_group.allow_tls.id
+  ]
+
+  target_groups = [
+    {
+      name_prefix      = "pref-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 }
 
