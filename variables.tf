4c2584778550bf0bf5faabab56f8f965c10e9d15
Alp Kocer <104270411+lucardcoder@users.noreply.github.com>
2022-11-25 20:45:19 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -13,6 +13,7 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
+
 variable "private_subnets" {
   default = {
     "private_subnet_1" = 1
@@ -28,4 +29,26 @@ variable "public_subnets" {
     "public_subnet_3" = 3
   }
 }
+
+variable "database_name" {
+  default = "wordpress"
+  type    = string
+}
+variable "database_password" {
+  default = "test1234"
+  type    = string
+}
+variable "database_user" {
+  default = "dbadmin"
+  type    = string
+}
+
+
+variable "instance_class" {
+  default = "db.t2.micro"
+  type    = string
+}
+
+variable "zone_id" {}
+variable "domain" {}
 
